b88bc7c788887f5baf01d2ff267943c03a999d59
hosin <didghtls11@gmail.com>
2024-10-04 10:55:27 +0900

    [Init] 프로젝트 생성

---
+++ backend/ricecake/src/main/java/project/ricecake/RicecakeApplication.java
@@ -0,0 +1,13 @@
+package project.ricecake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicecakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RicecakeApplication.class, args);
+    }
+
+}
