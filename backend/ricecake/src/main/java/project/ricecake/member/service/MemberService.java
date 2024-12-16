03d4addcb6e3fed368cbc25e061e59c676e13f17
hosin <didghtls11@gmail.com>
2024-12-16 20:19:27 +0900

    [Refactor] MemberService에 BaseResponse 기능 적용
    - 회원 가입과 로그인 기능에 각각 성공 응답과 실패 응답 기능 적용
    - 성공 응답에는 successResponse 기능 적용
    - 실패 응답에는 failResponse 기능 적용

--- backend/ricecake/src/main/java/project/ricecake/member/service/MemberService.java
+++ backend/ricecake/src/main/java/project/ricecake/member/service/MemberService.java
@@ -2,6 +2,7 @@ package project.ricecake.member.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.ricecake.common.BaseResponse;
 import project.ricecake.member.domain.request.PostLoginReq;
 import project.ricecake.member.domain.request.PostSignupReq;
 import project.ricecake.member.domain.entity.MemberEntity;
@@ -16,13 +17,19 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Object memberSignup(PostSignupReq postSignupReq) {
-        MemberEntity memberEntity = MemberEntity.buildMember(postSignupReq);
 
-        if (memberEntity != null) {
-            memberRepository.save(memberEntity);
-            return "created";
+        Optional<MemberEntity> findMember = memberRepository.findByMemberId(postSignupReq.getMemberId());
+
+        if (findMember.isPresent()) {
+            return BaseResponse.failResponse("MEMBER_ERROR_001", false, "중복된 회원", "fail");
         } else {
-            return null;
+            MemberEntity memberEntity = MemberEntity.buildMember(postSignupReq);
+            if (memberEntity != null) {
+                memberRepository.save(memberEntity);
+                return BaseResponse.successResponse("MEMBER_001", true, "회원가입 성공", "ok");
+            } else {
+                return BaseResponse.failResponse("MEMBER_ERROR_002", false, "회원가입 실패", "fail");
+            }
         }
     }
 
@@ -32,12 +39,12 @@ public class MemberService {
         if (findMember.isPresent()) {
             MemberEntity member = findMember.get();
             if (member.getMemberPw().equals(postLoginReq.getMemberPw())) {
-                return "ok";
+                return BaseResponse.successResponse("MEMBER_002", true, "로그인 성공", "ok");
             } else {
-                return "비밀번호가 틀립니다.";
+                return BaseResponse.failResponse("MEMBER_ERROR_004", false, "비밀번호 틀림", "fail");
             }
         }
 
-        return "회원이 존재하지 않습니다.";
+        return BaseResponse.failResponse("MEMBER_ERROR_003", false, "회원이 없음", "fail");
     }
 }
