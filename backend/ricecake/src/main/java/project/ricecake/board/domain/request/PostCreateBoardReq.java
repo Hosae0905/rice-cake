de8d6c48cbb972c45e590030578418619662fa9b
hosin <didghtls11@gmail.com>
2024-12-30 15:51:35 +0900

    [Refactor] PostCreateBoardReq DTO 필드에 검증 애노테이션 추가
    - 게시글 제목, 게시글, 회원 아이디에 대해서 공백과 notnull 처리를 위한 @NotBlank 애노테이션 추가
    - 게시글 제목, 게시글의 최대 글자수 제한을 위한 @Size 애노테이션 추가
    - 회원 아이디 정규표현식 검증을 위해 @Pattern 애노테이션 추가

--- backend/ricecake/src/main/java/project/ricecake/board/domain/request/PostCreateBoardReq.java
+++ backend/ricecake/src/main/java/project/ricecake/board/domain/request/PostCreateBoardReq.java
@@ -1,12 +1,24 @@
 package project.ricecake.board.domain.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class PostCreateBoardReq {
+
+    @NotBlank
+    @Size(max = 30)
     private String boardTitle;
+
+    @NotBlank
+    @Size(max = 500)
     private String boardContent;
+
+    @NotBlank
+    @Pattern(regexp = "^[a-z0-9]{8,20}$")
     private String memberId;
 }
