5828a93bb42d08994704703316d85899c31f2003
hosin <didghtls11@gmail.com>
2025-01-02 21:35:04 +0900

    [Refactor] PostSignupReq 요청 DTO에 메시지 속성을 지정 - 유효성 검증 실패시 응답으로 반환하기 위한 메시지 속성을 지정

--- backend/ricecake/src/main/java/project/ricecake/member/domain/request/PostSignupReq.java
+++ backend/ricecake/src/main/java/project/ricecake/member/domain/request/PostSignupReq.java
@@ -8,19 +8,20 @@ import lombok.Getter;
 @Getter
 public class PostSignupReq {
 
-    @NotBlank
-    @Pattern(regexp = "^[a-z0-9]{8,20}$")
+    @NotBlank(message = "회원 아이디는 공백일 수 없습니다.")
+    @Pattern(regexp = "^[a-z0-9]{8,20}$", message = "아이디 형식이 잘못되었습니다.")
     private String memberId;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-z0-9!@#$%^&*]{8,20}$")
+    @NotBlank(message = "회원 비밀번호는 공백일 수 없습니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-z0-9!@#$%^&*]{8,20}$", message = "비밀번호 형식이 잘못되었습니다.")
     private String memberPw;
 
-    @NotBlank
-    @Size(min = 2, max = 20)
+    @NotBlank(message = "회원 이름은 공백일 수 없습니다.")
+    @Size(min = 2, max = 20, message = "회원의 이름은 최소 2자부터 최대 20자까지 가능합니다.")
     private String memberName;
 
-    @NotNull
-    @Min(10) @Max(100)
+    @NotNull(message = "회원 나이는 공백일 수 없습니다.")
+    @Min(value = 10, message = "회원의 나이는 최소 10세부터 가능합니다.")
+    @Max(value = 100, message = "회원의 나이는 최대 100세까지 가능합니다.")
     private Integer memberAge;
 }
