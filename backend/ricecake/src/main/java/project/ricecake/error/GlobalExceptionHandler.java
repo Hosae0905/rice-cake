885b0d10f232fa5f88bea4cdce09695638267129
hosin <didghtls11@gmail.com>
2025-01-02 21:19:39 +0900

    [Feat] GlobalExceptionHandler 생성 - Spring AOP 를 적용하여 예외 처리 부분을 통합 관리 - Spring Validation 을 통한 유효성 검증에서 발생할 수 있는 예외를 처리할 수 있도록 MethodArgumentNotValidException 핸들러 생성 - 비즈니스 로직에서 발생할 수 있는 예외를 처리할 수 있도록 BusinessException 핸들러 생성 - 데이터 중복 예외 처리를 위한 DuplicateException 핸들러 생성 - 데이터 조회시 발생할 수 있는 예외를 처리하기 위한 NotFoundException 핸들러 생성 - 데이터 검증시 발생할 수 있는 예외를 처리하기 위한 UnauthorizedException 핸들러 생성 - 그 외 지정하지 않은 예외를 처리하기 위한 Exception 핸들러 생성

---
+++ backend/ricecake/src/main/java/project/ricecake/error/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package project.ricecake.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.ricecake.error.exception.BusinessException;
+import project.ricecake.error.exception.DuplicateException;
+import project.ricecake.error.exception.NotFoundException;
+import project.ricecake.error.exception.UnauthorizedException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        HttpStatusCode status = e.getStatusCode();
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), ErrorCode.INTERNAL_SERVER_ERROR, fieldError.getDefaultMessage());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = HttpStatus.BAD_GATEWAY;
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorCode);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(DuplicateException.class)
+    protected ResponseEntity<ErrorResponse> handleDuplicateException(DuplicateException e) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorCode);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorCode);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    protected ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException e) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorCode);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}
