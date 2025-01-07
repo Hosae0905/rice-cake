72f7d5ff8e6075f5b45f0e5a20b61ba369f3e22a
hosin <didghtls11@gmail.com>
2025-01-07 17:00:25 +0900

    [Refactor] 댓글 조회 서비스 로직 구현
    - 댓글 조회 기능의 서비스 로직을 구현
    - 페이지는 0부터 시작하기 때문에 받아온 페이지 값에서 1을 빼준 값으로 페이징 처리
    - 응답 객체를 리스트에 담아서 클라이언트에 반환

--- backend/ricecake/src/main/java/project/ricecake/comment/service/CommentService.java
+++ backend/ricecake/src/main/java/project/ricecake/comment/service/CommentService.java
@@ -1,11 +1,15 @@
 package project.ricecake.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import project.ricecake.board.domain.entity.BoardEntity;
 import project.ricecake.board.repository.BoardRepository;
 import project.ricecake.comment.domain.entity.CommentEntity;
 import project.ricecake.comment.domain.request.PostWriteCommentReq;
+import project.ricecake.comment.domain.response.GetCommentListRes;
 import project.ricecake.comment.repository.CommentRepository;
 import project.ricecake.common.BaseResponse;
 import project.ricecake.error.exception.notfound.BoardNotFoundException;
@@ -13,6 +17,8 @@ import project.ricecake.error.exception.notfound.UserNotFoundException;
 import project.ricecake.member.domain.entity.MemberEntity;
 import project.ricecake.member.repository.MemberRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -46,5 +52,29 @@ public class CommentService {
         CommentEntity comment = CommentEntity.buildComment(postWriteCommentReq, member, board);
         commentRepository.save(comment);
         return BaseResponse.successResponse("COMMENT_001", true, "댓글 작성 성공", "ok");
+    }
+
+    public Object getCommentList(int page, int size, Long boardIdx) {
+        Optional<BoardEntity> findBoard = boardRepository.findByBoardIdx(boardIdx);
+        List<GetCommentListRes> commentList = new ArrayList<>();
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        if (findBoard.isPresent()) {
+            BoardEntity board = findBoard.get();
+            Page<CommentEntity> comments = commentRepository.findAllByBoard(pageable, board);
+
+            for (CommentEntity comment : comments) {
+                commentList.add(GetCommentListRes.buildCommentListRes(
+                        comment.getMember().getMemberId(),
+                        comment.getCommentContent(),
+                        comment.getStartedAt()));
+            }
+
+        } else {
+            throw new BoardNotFoundException();
+        }
+
+        return BaseResponse.successResponse("COMMENT_002", true, "댓글 조회 성공", commentList);
     }
 }
