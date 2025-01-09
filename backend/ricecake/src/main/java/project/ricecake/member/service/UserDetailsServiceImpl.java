4f6bb5abc0affef38a8ecf9b40acfdcc546ccf10
hosin <didghtls11@gmail.com>
2025-01-09 20:26:57 +0900

    [Feat] UserDetailsService 인터페이스 구현체 생성
    - spring security에서 회원 정보를 활용할 수 있도록 UserDetailsService 인터페이스의 구현체 생성

---
+++ backend/ricecake/src/main/java/project/ricecake/member/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package project.ricecake.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import project.ricecake.error.exception.notfound.UserNotFoundException;
+import project.ricecake.member.domain.entity.MemberEntity;
+import project.ricecake.member.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Optional<MemberEntity> member = memberRepository.findByMemberId(memberId);
+
+        if (member.isPresent()) {
+            return member.get();
+        } else {
+            throw new UserNotFoundException();
+        }
+    }
+}
