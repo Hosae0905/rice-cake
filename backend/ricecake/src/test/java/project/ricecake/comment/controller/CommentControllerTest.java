a522b00f14bfb5992833f5675e04deda80a30fa8
hosin <didghtls11@gmail.com>
2025-01-14 16:29:51 +0900

    [Refactor] CommentControllerTest 코드 수정
    - 1-2 테스트에서 회원 아이디 공백을 검사하는 부분에서 알맞는 오류가 발생하지 않아 공백 문자열이 아닌 null을 넘겨줌으로 해결
    - 댓글 목록 조회 기능에 관한 성공 테스트 코드를 작성
    - 댓글 목록 조회 기능에서 특정 게시물이 없을 경우에 대한 실패 테스트 코드를 작성

--- backend/ricecake/src/test/java/project/ricecake/comment/controller/CommentControllerTest.java
+++ backend/ricecake/src/test/java/project/ricecake/comment/controller/CommentControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import project.ricecake.comment.domain.request.PostWriteCommentReq;
+import project.ricecake.comment.domain.response.GetCommentListRes;
 import project.ricecake.comment.service.CommentService;
 import project.ricecake.common.BaseResponse;
 import project.ricecake.config.WebSecurityConfig;
@@ -22,8 +22,12 @@ import project.ricecake.error.exception.notfound.UserNotFoundException;
 import project.ricecake.member.service.UserDetailsServiceImpl;
 import project.ricecake.utils.JwtUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -104,7 +108,7 @@ class CommentControllerTest {
     void writeFailInvalidMemberId() throws Exception {
 
         //given
-        PostWriteCommentReq postWriteCommentReq = new PostWriteCommentReq("test comment", "", 1L);
+        PostWriteCommentReq postWriteCommentReq = new PostWriteCommentReq("test comment", null, 1L);
 
         //when
         ResultActions resultActions = mockMvc.perform(
@@ -160,6 +164,64 @@ class CommentControllerTest {
                 post("/api/v1/comment/write")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(postWriteCommentReq))
+        );
+
+        //then
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value("404"))
+                .andExpect(jsonPath("$.code").value("BOARD_E001"))
+                .andExpect(jsonPath("$.message").value("게시글을 찾을 수 없습니다."))
+                .andDo(print());
+    }
+
+    @DisplayName("2. 댓글 목록 조회 성공")
+    @Test
+    void getCommentListSuccess() throws Exception {
+        //given
+        int page = 1;
+        int size = 10;
+        Long boardIdx = 1L;
+
+        GetCommentListRes getCommentListRes1 = GetCommentListRes.buildCommentListRes("member01", "first comment", LocalDateTime.now());
+        GetCommentListRes getCommentListRes2 = GetCommentListRes.buildCommentListRes("member02", "second comment", LocalDateTime.now());
+
+        List<GetCommentListRes> commentList = new ArrayList<>();
+        commentList.add(getCommentListRes1);
+        commentList.add(getCommentListRes2);
+
+        BaseResponse<Object> baseResponse = BaseResponse.successResponse("COMMENT_002", true, "댓글 조회 성공", objectMapper.writeValueAsString(commentList));
+
+        //when
+        given(commentService.getCommentList(page, size, boardIdx)).willReturn(baseResponse);
+
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/v1/comment/" + boardIdx)
+        );
+
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("COMMENT_002"))
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.message").value("댓글 조회 성공"))
+                .andExpect(jsonPath("$.result").value(objectMapper.writeValueAsString(commentList)))
+                .andDo(print());
+    }
+
+    @DisplayName("2-1. 댓글 목록 조회 시 특정 게시글이 없을 경우")
+    @Test
+    void getCommentListFailNotFoundBoard() throws Exception {
+        //given
+        int page = 1;
+        int size = 10;
+        Long boardIdx = 99L;
+
+        //when
+        given(commentService.getCommentList(page, size, boardIdx)).willThrow(new BoardNotFoundException());
+
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/v1/comment/" + boardIdx)
         );
 
         //then
