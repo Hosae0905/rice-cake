7a4b176f12cc8add10f3a24d64f35fbd719c18fc
hosin <didghtls11@gmail.com>
2025-01-16 16:25:57 +0900

    [Comment] MemberService 주석 추가

--- backend/ricecake/src/main/java/project/ricecake/member/service/MemberService.java
+++ backend/ricecake/src/main/java/project/ricecake/member/service/MemberService.java
@@ -17,24 +17,44 @@ import project.ricecake.utils.JwtUtils;
 
 import java.util.Optional;
 
+/**
+ * MemberService
+ * 회원에 관련된 기능들의 서비스 로직을 처리하기 위한 클래스
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtils jwtUtils;
+    private final PasswordEncoder passwordEncoder;      // 비밀번호 암호화를 위한 의존성 주입
+    private final JwtUtils jwtUtils;                    // JWT 기능을 사용하기 위한 의존성 주입
 
+    /**
+     * 회원가입 서비스 로직
+     * @param postSignupReq (회원가입 요청 DTO)
+     * @return 회원가입 성공 응답 객체(String)
+     * @throws UserDuplicateException : 중복된 아이디가 있을 경우
+     * @throws UserNotFoundException : 어떤 문제로 회원 생성이 안 될 경우
+     */
     @Transactional
     public BaseResponse<Object> memberSignup(PostSignupReq postSignupReq) {
 
         Optional<MemberEntity> findMember = memberRepository.findByMemberId(postSignupReq.getMemberId());
 
+        /**
+         * 만약 회원이 존재하면 UserDuplicateException 예외를 던진다.
+         * 그렇지 않으면 MemberEntity 객체를 생성한다.
+         */
         if (findMember.isPresent()) {
             throw new UserDuplicateException();
         } else {
             MemberEntity memberEntity = MemberEntity.buildMember(postSignupReq, passwordEncoder);
+
+            /**
+             * 만약 MemberEntity가 정상적으로 생성되었을 경우 DB에 저장하고 성공 응답을 반환한다.
+             * 그렇지 않을 경우 UserNotFoundException 예외를 던진다.
+             */
             if (memberEntity != null) {
                 memberRepository.save(memberEntity);
                 return BaseResponse.successResponse("MEMBER_001", true, "회원가입 성공", "ok");
@@ -44,11 +64,27 @@ public class MemberService {
         }
     }
 
+    /**
+     * 로그인 서비스 로직
+     * @param postLoginReq (로그인 요청 DTO)
+     * @return 로그인 성공 응답 객체(PostLoginRes)
+     * @throws InvalidPasswordException : 비밀번호가 일치하지 않을 경우
+     * @throws UserNotFoundException : 회원을 찾을 수 없을 경우
+     */
     public BaseResponse<Object> memberLogin(PostLoginReq postLoginReq) {
         Optional<MemberEntity> findMember = memberRepository.findByMemberId(postLoginReq.getMemberId());
 
+        /**
+         * 만약 회원이 존재할 경우 MemberEntity 객체를 가져온다.
+         * 그렇지 않을 경우 UserNotFoundException 예외를 던진다.
+         */
         if (findMember.isPresent()) {
             MemberEntity member = findMember.get();
+
+            /**
+             * 만약 요청으로 받은 비밀번호와 MemberEntity 객체의 비밀번호가 같으면 JWT 토큰을 생성하고 성공 응답을 반환한다.
+             * 그렇지 않을 경우 InvalidPasswordException 예외를 던진다.
+             */
             if (passwordEncoder.matches(postLoginReq.getMemberPw(), member.getPassword())) {
                 String token = jwtUtils.generateToken(member.getMemberIdx(), member.getMemberId());
                 return BaseResponse.successResponse("MEMBER_002", true, "로그인 성공", PostLoginRes.buildLoginRes(token));
