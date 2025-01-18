1d6167558cc1ca28a750ce2d2b1380de702a76d3
hosin <didghtls11@gmail.com>
2025-01-18 14:03:55 +0900

    [Refactor] 게시글 생성 요청 DTO 수정
    - 인증된 회원 아이디를 컨트롤러에서 따로 받기 때문에 게시글 생성 요청 DTO 필드에서 회원 아이디 필드를 제거

--- backend/ricecake/src/main/java/project/ricecake/board/domain/request/PostCreateBoardReq.java
+++ backend/ricecake/src/main/java/project/ricecake/board/domain/request/PostCreateBoardReq.java
@@ -21,8 +21,4 @@ public class PostCreateBoardReq {
     @NotBlank(message = "게시글은 공백일 수 없습니다.")
     @Size(max = 500, message = "게시글 내용은 최대 500자까지 가능합니다.")
     private String boardContent;
-
-    @NotBlank(message = "회원 아이디는 공백일 수 없습니다.")
-    @Pattern(regexp = "^[a-z0-9]{8,20}$", message = "아이디 형식이 잘못되었습니다.")
-    private String memberId;
 }
