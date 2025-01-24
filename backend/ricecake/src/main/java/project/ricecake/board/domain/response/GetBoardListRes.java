cfe35ae796f68563550c69b71a18bd76599b1db5
hosin <didghtls11@gmail.com>
2025-01-24 17:54:57 +0900

    [Refactor] 게시글 목록 조회 응답 DTO 수정
    - 게시글 목록에서 게시글 내용 필드를 제거
    - 게시글 생성 시간 필드를 추가
    - GetBoardListRes 객체 생성 시 게시글 생성 시간을 포함하도록 수정

--- backend/ricecake/src/main/java/project/ricecake/board/domain/response/GetBoardListRes.java
+++ backend/ricecake/src/main/java/project/ricecake/board/domain/response/GetBoardListRes.java
@@ -1,9 +1,12 @@
 package project.ricecake.board.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 /**
  * GetBoardListRes
@@ -17,26 +20,25 @@ public class GetBoardListRes {
     @Size(max = 30, message = "게시글 제목은 최대 30자까지 가능합니다.")
     private String boardTitle;
 
-    @NotBlank(message = "게시글은 공백일 수 없습니다.")
-    @Size(max = 500, message = "게시글 내용은 최대 500자까지 가능합니다.")
-    private String boardContent;
-
     @NotBlank(message = "회원 아이디는 공백일 수 없습니다.")
     @Size(min = 2, max = 20, message = "회원의 이름은 최소 2자부터 최대 20자까지 가능합니다.")
     private String memberName;
 
+    @NotBlank(message = "게시글 생성 시간은 공백일 수 없습니다.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime startedAt;
+
     /**
      * GetBoardListRes 객체 생성
      * @param boardTitle (게시글 제목)
-     * @param boardContent (게시글 내용)
      * @param memberName (작성자 회원 이름)
      * @return GetBoardListRes 객체
      */
-    public static GetBoardListRes buildBoardListRes(String boardTitle, String boardContent, String memberName) {
+    public static GetBoardListRes buildBoardListRes(String boardTitle, String memberName, LocalDateTime startedAt) {
         return GetBoardListRes.builder()
                 .boardTitle(boardTitle)
-                .boardContent(boardContent)
                 .memberName(memberName)
+                .startedAt(startedAt)
                 .build();
     }
 }
